0c1c6fa879c6249b80c25a3a3f8964d97ff351f7
Zach Weaver <r3dsierra@gmail.com>
2014-03-17 20:59:28 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shitstain::Application.config.secret_key_base = 'fb6c593570d7d35b33a3d081774130180083d5d1688b8610aa27c7b13fd85a567f475592409211b23f50f3f1041ed03864ff17c507a068c2a56d24bc434b61c1'
